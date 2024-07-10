9e2e278bb15cd87ff583191ec082fa467c0193a5
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-07-10 15:33:15 +0530

    .

--- reduxStore/src/components/Products.jsx
+++ reduxStore/src/components/Products.jsx
@@ -2,72 +2,101 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 const Products = () => {
-  //   const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  //   useEffect(() => {
-  //     const fetchProducts = async () = {
-  //         res = await fetch('https://fakestoreapi.com/products');
-  //         data = await res.json();
-  //         console.log(data);
-  //         setProducts(data);
-  //     }
-  //     fetchProducts();
-  //   }, [])
-  //   useEffect(() => {
-  //     const fetchProducts = (props) = {
-  //         res = fetch('https://fakestoreapi.com/products'),
-  //         data = res.json(),
-  //         console.log(data),
-  //         // setProducts(data),
-  //     }
-  //     fetchProducts();
-  //   }, [])
+  const fetchProducts = async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    console.log(data);
+    setProducts(data);
+  };
 
-  //   return <div>Products</div>;
-  const [posts, setPosts] = useState(null);
-  const targetUrl = 'https://www.reddit.com/r/blender.json';
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
 
-  const extractThumbnails = (data) =>
-    data.data.children
-      ?.filter((child) => child.data.thumbnail)
-      .map((post) => ({
-        image: post.data.thumbnail,
-        title: post.data.title,
-        link: post.data.url,
-      })) ?? null;
-
-  useEffect(() => {
-    const controller = new AbortController();
-    fetch(targetUrl, { signal: controller.signal, mode: 'no-cors' })
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts(extractThumbnails(data));
-      })
-      .catch((err) => {
-        if (err.name != 'AbortError') {
-          //   console.error(err);
-        }
-      });
-    return () => controller.abort();
-  }, []);
-  if (posts) {
-    return (
-      <>
-        <h1>Latest Posts</h1>
-        <ul className="image-list">
-          {posts.map(({ link, title, image }) => (
-            <li key={link}>
-              <a href={link} target="_blank" title={title}>
-                <img src={image} />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
+  function handleFetch() {
+    fetchProducts();
   }
+
+  // return <div>Products</div>;
+  return (
+    <div className="flex justify-center align-center ">
+      <div
+        style={{
+          width: '1200px',
+          padding: '20px',
+          margin: '20px',
+          border: '1px solid red',
+        }}
+      >
+        <p>Start editing to see some magic happen :)</p>
+        <p>Data Fecthing via an API</p>
+        <button onClick={handleFetch}>FETCH PRODUCTS</button>
+      </div>
+      {products.map((product) => {
+        return (
+          <>
+            <h2>{product.title}</h2>
+            <p key={product.id}>{product.category}</p>
+            <img
+              src={product.image}
+              alt="product image"
+              style={{ width: '100px', border: '1px solid red' }}
+            />
+            <p>{product.price}</p>
+            <hr />
+          </>
+        );
+      })}
+    </div>
+  );
 
   return <h1>Loading...</h1>;
 };
 
 export default Products;
+
+// TRY REDDIT API
+//   const [posts, setPosts] = useState(null);
+//   const targetUrl = 'https://www.reddit.com/r/blender.json';
+
+//   const extractThumbnails = (data) =>
+//     data.data.children
+//       ?.filter((child) => child.data.thumbnail)
+//       .map((post) => ({
+//         image: post.data.thumbnail,
+//         title: post.data.title,
+//         link: post.data.url,
+//       })) ?? null;
+
+//   useEffect(() => {
+//     const controller = new AbortController();
+//     fetch(targetUrl, { signal: controller.signal, mode: 'no-cors' })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setPosts(extractThumbnails(data));
+//       })
+//       .catch((err) => {
+//         if (err.name != 'AbortError') {
+//           //   console.error(err);
+//         }
+//       });
+//     return () => controller.abort();
+//   }, []);
+//   if (posts) {
+//     return (
+//       <>
+//         <h1>Latest Posts</h1>
+//         <ul className="image-list">
+//           {posts.map(({ link, title, image }) => (
+//             <li key={link}>
+//               <a href={link} target="_blank" title={title}>
+//                 <img src={image} />
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       </>
+//     );
+//   }
