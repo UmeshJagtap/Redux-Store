cd5424b0d72173a5a0de336c96a0990ac711564b
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-07-15 22:30:08 +0530

    .
    .

--- reduxTodo/src/App.jsx
+++ reduxTodo/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +30,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <hr />
+      <div>
+        <h1>Learn about redux toolkit</h1>
+        <AddTodo />
+        <Todos />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
