04b85c4cc877aa667b88e13d348e8aa231ac66fd
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-07-18 00:48:23 +0530

    updated todoSlice
    updated todoSlice

--- reduxTodo/src/features/todo/todoSlice.js
+++ reduxTodo/src/features/todo/todoSlice.js
@@ -3,3 +3,30 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 const initialState = {
   todos: [{ id: 1, text: 'Hello World' }],
 };
+
+// function syaHello() {
+//   console.log('Hello world');
+// }
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    // addTodo: () => {},
+    // removeTodo: () => {},
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload, // payload is a object
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
