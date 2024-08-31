51703a57bf1763fbeb0e8a5ee601b0f2fcd6c7db
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-08-31 21:01:57 +0530

    Zustand
    Zustand

--- zustand/zustand.js
+++ zustand/zustand.js
@@ -1,17 +1,22 @@
+// Install Zustand :- npm i zustand
+// Use Counter Component :-
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
 }));
 
-function Counter() {
-  const { count, inc } = useStore();
+export default function Counter() {
+  const { count, inc, dec } = useStore();
 
   return (
     <div>
       <span>{count}</span>
+      <br />
       <button onClick={inc}>one up</button>
+      <button onClick={dec}>one down</button>
     </div>
   );
 }
